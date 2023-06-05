9983be12d42faa82d3834858b7a509524be341c2
saman1357 <saman1357@yahoo.com>
2023-06-05 11:59:32 +0200

    Version 0

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,40 @@
+package org.example;
+class Student{
+    private String name;
+    private int birthYear;
+    private boolean enrolled;
+    public void setName(String settingName){
+        this.name=settingName;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public void setBirthYear(int settingBirthYear){
+        this.birthYear=settingBirthYear;
+    }
+    public int getBirthYear(){
+        return this.birthYear;
+    }
+    public void setEnrolled(boolean settingEnrolled){
+        this.enrolled=settingEnrolled;
+    }
+    public boolean getEnrolled(){
+        return this.enrolled;
+    }
+
+}
+public class Main {
+    public static void main(String[] args) {
+        Student me = new Student();
+        me.setName("Saman");
+        me.setBirthYear(1978);
+        me.setEnrolled(true);
+        System.out.println("Hello "+me.getName());
+        System.out.println("Year of Birth:  "+me.getBirthYear());
+        if (me.getEnrolled()) {
+            System.out.println("enrolled");
+        } else {
+            System.out.println("not enrolled");
+        }
+    }
+}
